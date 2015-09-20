1ddc75d583569855ff634b820ab8855145f82b2c
Shafiq Mohammed <shafiqdms1@gmail.com>
2015-09-20 17:49:07 -0400

    Created a full-blown UDP server with MD5 Authentication using a 4-step handshake between the client and server. Basically, the client requests to connect to the server. The server generates a 64-character string, and then sends it to the client. The client computes a hash with the username, password, and challenge string, and sends it to the server. The server then computes its own hash with the username and password stored in its so-called database, and checks the hash value. If they match, the user is authenticated, if they don't match, the user is not authenticated. Still a lot of work to do, including attaching a session key to the client and allowing the client to deposit or withdraw money from the clients respective account.

--- UDPClient.py
+++ UDPClient.py
@@ -1,5 +1,9 @@
 from socket import *
 import sys
+#importing the following allows us to use an MD5 Hash function
+#example where I learned how to use md5 hash code:  http://rosettacode.org/wiki/MD5#Python
+import hashlib
+
 serverName = 'localhost'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM);
@@ -8,13 +12,18 @@ x = 0
 username = sys.argv[1]
 password = sys.argv[2]
 
-
-#while(1):
 #Sending over username and password for authentication
-clientSocket.sendto(username,(serverName, serverPort))
-clientSocket.sendto(password,(serverName, serverPort))
+#clientSocket.sendto(username,(serverName, serverPort))
+#clientSocket.sendto(password,(serverName, serverPort))
+clientSocket.sendto("I want to connect!",(serverName, serverPort))
+challengeString, serverAddress = clientSocket.recvfrom(2048)
+print "received challenge!"
+#Client has now received challenge, computes hash and sends username and hash to server:
+#Note: Must send hash as a string because of sendto function limitations, so sent as string
+hashOfChallenge = hashlib.md5(username + password + challengeString)
+clientSocket.sendto(username, (serverName, serverPort))
+clientSocket.sendto(hashOfChallenge.hexdigest(), (serverName, serverPort))
+print "Sent user and hash to server!"
 
 
-receivedMsg, serverAddress = clientSocket.recvfrom(2048)
-print receivedMsg;
 clientSocket.close()
--- UDPServer.py
+++ UDPServer.py
@@ -1,21 +1,73 @@
 from socket import *
+import string
+import random
+import hashlib
+
+#Specifies the port number of this server, creates a socket, and then binds a port
+#number to it, making this a server socket.
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM);
 serverSocket.bind(('', serverPort))
-correctUsername = "Shafiq"
-correctPassword = "Meow"
 
+#Made a user class to contain 3 users:
+class User(object):
+	username = ""
+	password = ""
+	balance = ""
+
+#Populate the class with users
+userOne = User()
+userOne.username = "Shafiq"
+userOne.password = "Cat"
+userOne.balance = 100;
+
+userTwo = User()
+userTwo.username = "Huda"
+userTwo.password = "TaubaHain"
+userTwo.balance = 1000;
+
+userThree = User()
+userThree.username = "Sheeni"
+userThree.password = "Kitty"
+userThree.balance = 10000;
+
+#Store all three users in one wanna-be database
+hardcodedDB = [userOne, userTwo, userThree]
 
 print "The server is ready to receive!"
 
+while(1):
+	request, clientAddress = serverSocket.recvfrom(2048)
+	if (request == "I want to connect!"):
+		print "I see you wanna connect"
+		#Generates one-time use challenge value in the form of a 64 char string:
+		challengeString = ''.join(random.choice(string.lowercase) for i in range(64))
+		#Sends string to client:
+		serverSocket.sendto(challengeString, clientAddress)
+		print "Challenge string sent!: " + challengeString
+		#Receives computed hash in the form of a string
+		usernameFromClient, clientAddress = serverSocket.recvfrom(2048)
+		hashOfChallenge, clientAddress = serverSocket.recvfrom(2048)
+		print "DEBUG: username received is: " + usernameFromClient
+		#Server computes hash value with legit username and checks value with hash received
+		userFound = False
+		for user in hardcodedDB:
+			if user.username == usernameFromClient:
+				legitHash = hashlib.md5(user.username + user.password + challengeString)
+				userFound = True
+		#If username don't exist in DB, then you exit session with user.
+		#TODO: Just randomly coded this for now, come back to it and fix this ish
+		if userFound != True:
+			print "Username was not found in our database"
+			
 
-providedUsername, clientAddress = serverSocket.recvfrom(2048)
-providedPassword, clientAddress = serverSocket.recvfrom(2048)
+		if (legitHash.hexdigest() == hashOfChallenge):
+			print "YO MAH NIGGA " + usernameFromClient + " YOU HAZ BEEN AUTHENTICATATED TURN UP!!!"
+		else:
+			print "Incorrect pass. You iz stoop, get out my server."
 
-if providedUsername == correctUsername and providedPassword == correctPassword:
-	authMessage = "You have been authentiated"
-else:
-	authMessage = "You are a fake"
 
-serverSocket.sendto(authMessage, clientAddress)
-serverSocket.close();
+
+
+
+serverSocket.close()
